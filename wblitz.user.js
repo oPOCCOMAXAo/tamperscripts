2073da1a950d36742f0c6c67dfb12131fda9d423
oPOCCOMAXAo <oPOCCOMAXAo@users.noreply.github.com>
2019-06-22 20:33:56 +0300

    Create wblitz.user.js

---
+++ wblitz.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         wblitz stream collector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  run blitz stream
+// @author       oPOCCOMAXAo
+// @match        https://wotblitz.ru/*
+// @grant        none
+// ==/UserScript==
+
+let url = "wss://wotblitz.ru/tournament-season/watch/88fd5f322a3743909f32de9ede94422d/";
+let ws = null;
+function start(){
+    ws = new WebSocket(url);
+    ws.onopen = onopen;
+    ws.onmessage = onmessage;
+}
+function send(){
+    ws.send('{\"command\":\"watching\"}');
+}
+function onopen(){
+    ws.send('{\"command\":\"close\"}');
+}
+function onmessage(msg){
+    setTimeout(send, 1000);
+    let t = JSON.parse(msg.data).data;
+    document.title = Math.round(JSON.parse(msg.data).data / 60) * 500 + " " + t;
+}
+window.start = start;
+start();
