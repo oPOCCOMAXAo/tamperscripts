addd37bfce096c7b8c098a9116087dde9f558518
oPOCCOMAXAo <oPOCCOMAXAo@users.noreply.github.com>
2019-09-26 19:35:33 +0300

    Update wblitz.user.js

--- wblitz.user.js
+++ wblitz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wblitz stream collector
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  run blitz stream
 // @author       oPOCCOMAXAo
 // @match        https://wotblitz.ru/*
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-let url = "wss://wotblitz.ru/tournament-season/watch/62180f0566104dfdb9caad0b1eb4f7f6/";
+let url = "wss://wotblitz.ru/tournament-season/watch/39368a1761d74819ad904f1837b1a351/";
 let ws = null;
 function start(){
     ws = new WebSocket(url);
