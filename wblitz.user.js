a1d0e1b6992a395208b3bea4ab9ad6cae09b0ea7
POCCOMAXA <oPOCCOMAXAo@users.noreply.github.com>
2020-11-01 08:56:09 +0200

    Update wblitz.user.js

--- wblitz.user.js
+++ wblitz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wblitz stream collector
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.12
 // @description  run blitz stream
 // @author       oPOCCOMAXAo
 // @match        https://ru.wotblitz.com/*
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-let url = "wss://watchblitz-ru.wotblitz.com/v1/groups/3/watch?token=4ba160b2-7561-46e4-ab9f-92286e4a3beb";
+let url = "wss://watchblitz-ru.wotblitz.com/v1/groups/2/watch?token=d36347df-8eee-434b-815f-017bad75db93";
 let ws = null;
 function start(){
     ws = new WebSocket(url);
