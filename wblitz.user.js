77aeb3e90cd9cdf11e8f2298980ac4498b2f15ea
oPOCCOMAXAo <opoccomaxao@gmail.com>
2020-11-01 10:18:24 +0200

    fully automated stream collector

--- wblitz.user.js
+++ wblitz.user.js
@@ -1,27 +1,58 @@
 // ==UserScript==
 // @name         wblitz stream collector
 // @namespace    http://tampermonkey.net/
-// @version      0.12
+// @version      0.13
 // @description  run blitz stream
 // @author       oPOCCOMAXAo
 // @match        https://ru.wotblitz.com/*
 // @exclude      https://ru.wotblitz.com/*tournament*
+// @require      https://raw.githubusercontent.com/oPOCCOMAXAo/jslib/master/asyncxhr.js
 // @grant        none
 // ==/UserScript==
+/*global AsyncXHR*/
 
-let url = "wss://watchblitz-ru.wotblitz.com/v1/groups/2/watch?token=d36347df-8eee-434b-815f-017bad75db93";
-let ws = null;
-function start(){
-    ws = new WebSocket(url);
-    ws.onopen = onopen;
-    ws.onmessage = onmessage;
-}
-function send() { ws.send('{\"operation\":\"watching\"}'); }
-function onopen() { ws.send('{\"operation\":\"close\"}'); }
-function onmessage(msg) {
+function watch(url) {
+  let ws = new WebSocket(url);
+  let timer;
+  ws.onopen = ws.send.bind(ws, "{\"operation\":\"close\"}");
+  ws.onmessage = msg => {
     let t = JSON.parse(msg.data).time;
     document.title = t + " сек";
+  };
+  ws.onclose = () => {
+    clearInterval(timer);
+    watch(url);
+  };
+  timer = setInterval(ws.send.bind(ws, "{\"operation\":\"watching\"}"), 4500);
 }
-setInterval(send, 2000);
+
+async function getToken() {
+  let res = await AsyncXHR.request("POST", "https://ru.wotblitz.com/ru/api/watch-blitz/v1/user/", "{}", { "Content-Type": "application/json" });
+  return res.object.token;
+}
+
+async function getStreams() {
+  return (await AsyncXHR.get("https://ru.wotblitz.com/ru/api/watch-blitz/v1/streams")).object;
+}
+
+async function start() {
+  // получение токена, проверка авторизации
+  let token = await getToken();
+  if (token == null) {
+    alert("Авторизуйся и обнови");
+    return;
+  }
+
+  // получение стримов, фильтр нужных
+  let stream = (await getStreams()).result.filter(s => s.end_at == null)[0];
+  if (stream == null) {
+    alert("Сорян, стрима нет");
+    return;
+  }
+
+  // просмотр стрима
+  watch(`wss://watchblitz-ru.wotblitz.com/v1/groups/${stream.stream_group_id}/watch?token=${token}`);
+}
+
 window.start = start;
-start();
+start().catch(console.error);
