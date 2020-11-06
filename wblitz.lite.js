49ad5c32696c151d8e2c88dbac22ce273b4808d8
oPOCCOMAXAo <opoccomaxao@gmail.com>
2020-11-06 16:02:08 +0200

    improved stability, updated messages text

--- wblitz.lite.js
+++ wblitz.lite.js
@@ -1,21 +1,40 @@
-function getToken() {
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+function request(url, data) {
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://ru.wotblitz.com/ru/api/watch-blitz/v1/user/");
+  xhr.open("POST", url);
   xhr.setRequestHeader("Content-Type", "application/json");
   return new Promise(r => {
-    xhr.onload = () => r(JSON.parse(xhr.responseText).token);
-    xhr.send("{}");
+    xhr.onload = () => r({
+      text: xhr.responseText,
+      status: xhr.status,
+    });
+    xhr.onerror = () => r({
+      object: {},
+      status: -1,
+    });
+    xhr.send(data);
   });
 }
 
-function sendToken(token) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://wbstream.mircloud.host/watch");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  return new Promise(r => {
-    xhr.onload = () => r(xhr.responseText == "ok");
-    xhr.send(`{"token":"${token}"}`);
-  });
+async function getToken() {
+  while (true) {
+    let res = await request("https://ru.wotblitz.com/ru/api/watch-blitz/v1/user/", "{}");
+    if (res.status === 200) {
+      return JSON.parse(res.text).object.token;
+    }
+    await sleep(5000);
+  }
+}
+
+async function sendToken(token) {
+  while (true) {
+    let res = await request("https://wbstream.mircloud.host/watch", `{"token":"${token}"}`);
+    if (res.status === 200) {
+      return res.text === "ok";
+    }
+    await sleep(5000);
+  }
 }
 
 async function start() {
@@ -28,9 +47,9 @@ async function start() {
 
   // отправка токена
   if (await sendToken(token)) {
-    alert("Всё ок!");
+    alert("Всё ок, можешь закрывать вкладку");
   } else {
-    alert("Что-то пошло не так");
+    alert("Что-то пошло не так, напиши в дискорд");
   }
 }
 
